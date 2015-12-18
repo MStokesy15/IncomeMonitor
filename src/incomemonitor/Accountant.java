f7c548be80bf9be1e00d8f87af84312e561680f6
Matt <matthewstokes@Matthews-MacBook-Pro.local>
2015-12-18 22:37:06 +0000

    Initial commit

---
+++ src/incomemonitor/Accountant.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package incomemonitor;
+
+import java.util.*;
+/**
+ *
+ * @author matthewstokes
+ */
+public class Accountant {
+    List<Creditor> creditors;
+    Calendar calendar = Calendar.getInstance();
+    
+    public Accountant() {
+        creditors = new ArrayList<>();
+    }
+    
+    public final static Accountant getAccountant(){
+        return new Accountant();
+    }
+    
+    public void addCreditor (String aName, String aDate, double anAmount, double aBalance) {
+        creditors.add(new Creditor(aName, aDate, anAmount, aBalance));
+    }
+    
+    public Creditor getCreditor(int anIndex) {
+        return creditors.get(anIndex);
+    }
+    
+    public Collection getCreditors(){
+        return new ArrayList<>(creditors);
+    }
+    
+    public String[] getDOM(){
+        String[] maxDays = new String[31];
+        
+        for (int i=0; i<31; i++){
+            maxDays[i] = "" + (i + 1);
+        }
+        
+        return maxDays;
+    }
+    
+    public void removeCreditor(Creditor aCreditor){
+        creditors.remove(aCreditor);
+    }
+}
---
+++ src/incomemonitor/Creditor.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package incomemonitor;
+
+/**
+ *Objects of class Creditor represent creditors.
+ * 
+ * @author matthewstokes
+ */
+public class Creditor {
+    String name = "";
+    String date = "";
+    double amount = 0;
+    double balance = 0;
+    boolean status;
+            
+    Creditor (String aName, String aDate, double anAmount, double aBalance) {
+        name = aName;
+        date = aDate;
+        amount = anAmount;
+        balance = aBalance;
+        status = false;
+    }
+            
+public void changeStatus()
+{
+    if (status)
+    {
+        status = false;
+    }
+    else
+    {
+        status = true;
+    }
+}
+
+@Override
+public String toString(){
+    return name;
+}
+
+}
